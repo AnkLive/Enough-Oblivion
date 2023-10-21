da1ba44fd6bce3d72d8893b41d5b5891ef981237
Alexander <bionqine@gmail.com>
2023-10-21 15:24:35 +0200

    add: update system

---
+++ Assets/Scripts/Installers/InitializeInstaller.cs
@@ -0,0 +1,26 @@
+﻿using Zenject;
+
+public interface ISubscriber<T>
+{
+    public void Subscribe();
+    public void Unsubscribe();
+}
+
+public interface IActivate<T>
+{
+    public void Activate();
+    public void Deactivate();
+}
+
+public interface IInitialize<T>
+{
+    public void Initialize();
+}
+
+public class InitializeInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/System/GlobalUpdate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GlobalUpdate : MonoBehaviour
+{
+    private void Update()
+    {
+        for (int i = 0; i < MonoCache.AllUpdate.Count; i++) MonoCache.AllUpdate[i].Tick();
+    }
+    
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < MonoCache.AllFixedUpdate.Count; i++) MonoCache.AllFixedUpdate[i].FixedTick();
+    }
+    
+    private void LateUpdate()
+    {
+        for (int i = 0; i < MonoCache.AllLateUpdate.Count; i++) MonoCache.AllLateUpdate[i].LateTick();
+    }
+}
