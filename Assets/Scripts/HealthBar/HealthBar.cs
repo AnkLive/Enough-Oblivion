8a2a1d600dc84e0ab266f94cbdde1758e58f0aa9
SelfishBear <danil16013@gmail.com>
2023-11-26 14:12:25 +0100

    healthUpdate
    heaalth

---
+++ Assets/Scripts/HealthBar/HealthBar.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+     public int playerHealth;
+     public int amountHealth;
+     public Image[] hearts;
+     public Sprite FullH;
+     public Sprite EmptyH;
+   
+}
+
